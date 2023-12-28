0015196fa6edbb9061ccb94b3f5f867c2f43bfc8
shifa <fathimashifasalim@gmail.com>
2023-12-28 10:17:51 +0530

    message

---
+++ api/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+class Employee(models.Model):
+    name=models.CharField(max_length=200)
+    email=models.EmailField()
+    department=models.CharField(max_length=200)
+    salary=models.PositiveIntegerField()
+    age=models.PositiveBigIntegerField()
+    contact=models.CharField(max_length=200,null=True)
+
+    def __str__(self) :
+        return self.name
